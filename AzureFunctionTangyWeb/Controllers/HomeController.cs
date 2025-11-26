b2b47694311cb996d98d5ba6930893fdc45f28d7
DarshanPawar23 <darshansp0230@gmail.com>
2025-11-26 14:43:30 +0530

    added azure function

--- AzureFunctionTangyWeb/Controllers/HomeController.cs
+++ AzureFunctionTangyWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using AzureFunctionTangyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace AzureFunctionTangyWeb.Controllers
@@ -7,16 +8,35 @@ namespace AzureFunctionTangyWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+        // http://localhost:7172/api/OnSalesUploadWriteToQueue
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SalesRequest salesRequest)
+        {
+            salesRequest.Id = Guid.NewGuid().ToString();
+            using var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("http://localhost:7172/api/");
+
+            using (var content = new StringContent(JsonConvert.SerializeObject(salesRequest), System.Text.Encoding.UTF8, "application/json"))
+            {
+                HttpResponseMessage httpResponseMessage = await client.PostAsync("OnSalesUploadWriteToQueue", content);
+                string returnValue = await httpResponseMessage.Content.ReadAsStringAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        } 
 
         public IActionResult Privacy()
         {
